00343d3c3d8908e4317213218225c7e41d85b94a
abaasasad <abaassaveman@gmail.com>
2021-10-29 22:01:29 +0300

    updated

--- js/script.js
+++ js/script.js
@@ -14,32 +14,40 @@ $(document).ready(function() {
     var ques6 = document.forms["questions"]["question6"].value;
     var ques7 = document.forms["questions"]["question7"].value;
     var ques8 = document.forms["questions"]["question8"].value;
+    var ques9 = document.forms["questions"]["question8"].value;
+    var ques10 = document.forms["questions"]["question8"].value;
 
 
 
          if(ques1 == "answer"){
-            score = marks + 12.5;
+            score = marks + 10;
           }
           if(ques2 == "answer"){
-            score = score+12.5;
+            score = score+10;
           }
           if(ques3 == "answer"){
-            score = score + 12.5;
+            score = score + 10;
           }
           if(ques4 == "answer"){
-            score = score + 12.5;
+            score = score + 10;
           }
           if(ques5 == "answer"){
-            score = score + 12.5;
+            score = score + 10;
           }
           if(ques6 == "answer"){
-            score = score + 12.5;
+            score = score + 10;
           }
           if(ques7 == "answer"){
-            score = score + 12.5;
+            score = score + 10;
           }
           if(ques8 == "answer"){
-            score = score + 12.5;
+            score = score + 10;
+          }
+          if(ques9 == "answer"){
+            score = score + 10;
+          }
+          if(ques10 == "answer"){
+            score = score + 10;
           }
 
        event.preventDefault();
